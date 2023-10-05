2e66539453e29767fb7b44b8d42919661b9b294a
Román Cárdenas <rcardenas.rod@gmail.com>
2023-10-05 11:30:43 +0200

    trying to make GDB work

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,6 +9,12 @@ sudo apt install cmake -y
 # Install openocd
 sudo apt install openocd -y
 
+# Install depedencies for arm toolchain
+sudo apt install libncurses-dev -y
+# Create symbolic links to libncurses.so.5 and libtinfo.so.5 to force arm toolchain to use libncurses.so.6 and libtinfo.so.6
+sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5
+
 # Download arm toolchain zip file
 wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz -O /tmp/arm-toolchain.tar.xz
 
