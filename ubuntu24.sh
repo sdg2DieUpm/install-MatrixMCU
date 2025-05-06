5fbb9f83947a37b01e78a66afeedf394acae9f25
Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>
2025-05-06 08:53:51 +0200

    support for aarch64 in Linux

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -3,33 +3,35 @@
 # Update package list
 sudo apt update
 
-# Install build-essential
-sudo apt install build-essential
+# Install dependencies
+sudo apt install -y build-essential cmake openocd gdb-multiarch
 
-# Install cmake
-sudo apt install cmake
-# sudo snap install cmake --classic
-
-# Install openocd
-sudo apt install openocd
-
-# Install gdb multiarch
-sudo apt install gdb-multiarch
-
-# Create directories for the tools used in MatrixMCU course
+# Create directories for the tools used in MatrixMCU toolkit
 sudo mkdir -p /opt/MatrixMCU/bin
 
+# Detect required toolchain binary depending on host architecture
+TOOLCHAIN_VERSION="15.2.rel1"
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    TOOLCHAIN_DIR="arm-gnu-toolchain-$TOOLCHAIN_VERSION-x86_64-arm-none-eabi"
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    TOOLCHAIN_DIR="arm-gnu-toolchain-$TOOLCHAIN_VERSION-aarch64-arm-none-eabi"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 # Download arm toolchain zip file
-wget https://developer.arm.com/-/media/Files/downloads/gnu/15.2.rel1/binrel/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi.tar.xz -O /tmp/arm-toolchain.tar.xz
+wget https://developer.arm.com/-/media/Files/downloads/gnu/$TOOLCHAIN_VERSION/binrel/$TOOLCHAIN_DIR.tar.xz -O /tmp/arm-toolchain.tar.xz
 # Uncompress arm toolchain zip file in /opt directory
 sudo tar -xf /tmp/arm-toolchain.tar.xz -C /opt/MatrixMCU
 
 # Create symbolic links to arm toolchain in /opt/MatrixMCU/bin
-sudo ln -s /opt/MatrixMCU/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /opt/MatrixMCU/bin/arm-none-eabi-gcc
-sudo ln -s /opt/MatrixMCU/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /opt/MatrixMCU/bin/arm-none-eabi-g++
-sudo ln -s /opt/MatrixMCU/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-nm /opt/MatrixMCU/bin/arm-none-eabi-nm
-sudo ln -s /opt/MatrixMCU/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy /opt/MatrixMCU/bin/arm-none-eabi-objcopy
-sudo ln -s /opt/MatrixMCU/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objdump /opt/MatrixMCU/bin/arm-none-eabi-objdump
+sudo ln -s /opt/MatrixMCU/$TOOLCHAIN_DIR/bin/arm-none-eabi-gcc /opt/MatrixMCU/bin/arm-none-eabi-gcc
+sudo ln -s /opt/MatrixMCU/$TOOLCHAIN_DIR/bin/arm-none-eabi-g++ /opt/MatrixMCU/bin/arm-none-eabi-g++
+sudo ln -s /opt/MatrixMCU/$TOOLCHAIN_DIR/bin/arm-none-eabi-nm /opt/MatrixMCU/bin/arm-none-eabi-nm
+sudo ln -s /opt/MatrixMCU/$TOOLCHAIN_DIR/bin/arm-none-eabi-objcopy /opt/MatrixMCU/bin/arm-none-eabi-objcopy
+sudo ln -s /opt/MatrixMCU/$TOOLCHAIN_DIR/bin/arm-none-eabi-objdump /opt/MatrixMCU/bin/arm-none-eabi-objdump
 
 # Get the path to gdb-multiarch...
 GDB_MULTIARCH_PATH=$(which gdb-multiarch)
